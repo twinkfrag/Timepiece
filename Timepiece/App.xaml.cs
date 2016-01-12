561940ed25532815632d6c3f8628ccebd48172b6
あると <mail@twinkfrag.net>
2016-01-12 17:44:37 +0900

    とりあえずNotifyIconが出るプロジェクト作成

---
+++ Timepiece/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Timepiece
+{
+	/// <summary>
+	/// App.xaml の相互作用ロジック
+	/// </summary>
+	public partial class App : Application
+	{
+		private TaskTrayIcon taskTrayIcon;
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			taskTrayIcon = new TaskTrayIcon();
+		}
+
+		protected override void OnExit(ExitEventArgs e)
+		{
+			base.OnExit(e);
+
+			taskTrayIcon.Dispose();
+		}
+	}
+}
---
+++ Timepiece/TaskTrayIcon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Timepiece
+{
+	class TaskTrayIcon : IDisposable
+	{
+		private readonly NotifyIcon notifyIcon;
+
+		public TaskTrayIcon()
+		{
+			notifyIcon = new NotifyIcon
+			{
+				Text = nameof(Timepiece),
+				Icon = SystemIcons.Application,
+				Visible = true,
+				ContextMenu = new ContextMenu(new[]
+				{
+					new MenuItem("E&xit", (sender, args) => App.Current.Shutdown()),
+				}),
+			};
+		}
+
+		public void Dispose()
+		{
+			notifyIcon?.Dispose();
+		}
+	}
+}
