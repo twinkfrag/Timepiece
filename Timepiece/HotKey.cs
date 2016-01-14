25a564f730ca33648094b2d7bdbaee8be1bfa0a7
あると <mail@twinkfrag.net>
2016-01-14 15:42:15 +0900

    HotKeyクラスの追加

---
+++ Timepiece/HotKey.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Timepiece
+{
+	public class HotKey : IDisposable
+	{
+		private readonly IntPtr hWnd;
+		private readonly IntPtr lParam;
+
+		public HotKey(IntPtr hWnd, ModKeys modKey, Keys key)
+		{
+			this.hWnd = hWnd;
+			lParam = new IntPtr((int)modKey | ((int)key * 0x10000));
+
+			NativeMethods.RegisterHotKey(hWnd, NativeMethods.WM_HOTKEY, modKey, key);
+		}
+
+		public void Dispose()
+		{
+			NativeMethods.UnregisterHotKey(hWnd, NativeMethods.WM_HOTKEY);
+		}
+
+		public bool IsHotKeyDown(int msg, IntPtr hotKeyLParam)
+		{
+			return (msg == NativeMethods.WM_HOTKEY) && (hotKeyLParam == lParam);
+		}
+
+
+		private static class NativeMethods
+		{
+			private const string User32Dll = "user32.dll";
+
+			[DllImport(User32Dll)]
+			public static extern int RegisterHotKey(IntPtr hWnd, int id, ModKeys modKey, Keys key);
+
+			[DllImport(User32Dll)]
+			public static extern int UnregisterHotKey(IntPtr hWnd, int id);
+
+			public const int WM_HOTKEY = 0x0312;
+		}
+	}
+
+	[Flags]
+	public enum ModKeys
+	{
+		Alt = 0x0001,
+		Control = 0x0002,
+		Shift = 0x0004,
+		Win = 0x0008,
+	}
+}
