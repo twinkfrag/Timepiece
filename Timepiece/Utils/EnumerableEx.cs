de3160a4facd8f09214ec57f4f5d7b538ce56d41
あると <mail@twinkfrag.net>
2016-02-01 14:24:25 +0900

    JoinToStringを追加

---
+++ Timepiece/Utils/EnumerableEx.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace twinkfrag.Timepiece.Utils
+{
+	public static class EnumerableEx
+	{
+		public static string JoinToString<T>(this IEnumerable<T> values, string separator)
+			=> string.Join(separator, values);
+
+		public static string JoinToString(this IEnumerable<string> values, string separator)
+			=> string.Join(separator, values);
+	}
+}
