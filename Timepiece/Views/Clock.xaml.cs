1386044a780b485e9686305492b16388d140c9a2
あると <mail@twinkfrag.net>
2016-02-12 21:30:12 +0900

    Windowの位置なんかはコードビハインドに書いた方がよさそう

--- Timepiece/Views/Clock.xaml.cs
+++ Timepiece/Views/Clock.xaml.cs
@@ -11,9 +11,16 @@ namespace twinkfrag.Timepiece.Views
 		public Clock()
 		{
 			InitializeComponent();
+
+			var desktop = System.Windows.Forms.Screen.FromPoint(System.Windows.Forms.Cursor.Position).Bounds;
+			this.Left = desktop.Left + DISPLAY_MARGIN;
+			this.Top = desktop.Bottom - this.Height - DISPLAY_MARGIN;
+
 			this.Show();
 			this.Activate();
 		}
+
+		private const double DISPLAY_MARGIN = 100d;
 
 		private void Clock_OnDeactivated(object sender, EventArgs e)
 		{
